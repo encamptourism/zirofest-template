93b3076e5da12e7936407d63b60dee00188732bf
Jitumoni changkakoty <g2.genious@gmail.com>
2022-07-14 13:17:58 +0530

    cart pane changes done

--- components/common/cartpane.jsx
+++ components/common/cartpane.jsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 const Cartpane=(props)=>{
 const {addtocartdata,setAddtocartdata} = props;
 const [cartNumber,setCartnumber] = useState(0);
-
+const [total,setTotal] = useState(0);
 useEffect(()=>{
+let totcart = 0;
 if(addtocartdata){
 if(addtocartdata.length > 0){
- setCartnumber(addtocartdata.length);   
+ setCartnumber(addtocartdata.length);
+
+addtocartdata.map((data)=>{
+ totcart = +totcart + data.packageprice;   
+});
+setTotal(totcart);
 }
 
 }
@@ -20,7 +26,11 @@ return (
  <>
 {cartNumber && cartNumber > 0 ? 
 <div  className="flexcontainer">
-<div className="aadga" style={{width:"100%"}}>{cartNumber} items selected.<Link href="/cart"><a className="bg-gradient-to-r from-green-600 to-green-500 rounded-full py-1 px-3 text-gray-50 text-sm md:self-start">click here</a></Link></div>
+<div className="aadga">
+<div style={{fontSize:"0.9rem"}}>{cartNumber} item.</div>
+<div style={{fontSize:"0.9rem"}}>Rs.{total} / + Taxes</div>
+</div>
+<div className="aadga" style={{marginTop:"0.5rem"}}><Link href="/cart"><a className="bg-gradient-to-r from-green-600 to-green-500 rounded-full  px-6 text-gray-50 text-sm md:self-start">Cart</a></Link></div>
 </div>:""}
 </>
 )
