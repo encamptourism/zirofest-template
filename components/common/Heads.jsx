ba62fbf53bed36949df64131d51e4d6cac903bf6
Jitumoni changkakoty <g2.genious@gmail.com>
2022-07-22 11:27:02 +0530

    favicon description added

--- components/common/Heads.jsx
+++ components/common/Heads.jsx
@@ -5,7 +5,10 @@ const Heads =()=>{
     <meta charSet="UTF-8"/>
     <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="If you want to travel in Northeast India, talk to Encamp Adventures - the leading camping service providers in Assam, Meghalaya, Arunachal Pradesh, Nagaland; we offer customized trips in the region."/>
+    <meta name="keywords" content="Encamp Adventures, camping, camping provider, luxury camping, camping places, camping trip, adventure, adventure tourism, trekking, trekking in india, north east india tourism, kayaking, cliff jumping, recreational activities"/>
     <title>Ziro Festival of Music : Encamp Adventures</title>
+    <link rel="icon" type="image/png" href="/images/icons/favicon-32x32.png" sizes="32x32"/>
      </Head>
           )
 }
