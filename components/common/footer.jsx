ed41dda51dfd7c5139dcf7c2cf6e98745ecc2924
Jitumoni changkakoty <g2.genious@gmail.com>
2022-07-23 13:40:29 +0530

    social icons added

--- components/common/footer.jsx
+++ components/common/footer.jsx
@@ -1,3 +1,4 @@
+import { SocialIcon } from 'react-social-icons';
 const Footer=()=>{
 return (
 	    <>
@@ -9,7 +10,12 @@ return (
                  <div className="text-center font-semibold text-sm">+91-8876698046</div>
                 <div></div>
                 </div>
-        
+        <div className="flex flex-col-4 gap-4 justify-center">
+        <SocialIcon style={{width:'30px',height:"30px"}} url="https://www.facebook.com/encampadventures/"/>
+        <SocialIcon style={{width:'30px',height:"30px"}} url="https://www.instagram.com/encampadventures/"/>
+        <SocialIcon style={{width:'30px',height:"30px"}} url="https://www.youtube.com/channel/UCKT1ZC-dMCQr3fBTr57rKAQ"/>
+        <SocialIcon style={{width:'30px',height:"30px"}} label="google" url="https://tinyurl.com/yx8nblk6"/>
+        </div>
 	     </div>	
 	    </>
 	    )
