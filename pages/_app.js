1297d0f5a535a3b1822d8ff9d93f326aa556f2ef
Jitumoni changkakoty <g2.genious@gmail.com>
2022-07-24 14:00:58 +0530

    version control is attached

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { useHasNewDeploy } from "next-deploy-notifications";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  let { hasNewDeploy } = useHasNewDeploy();
+  return (
+    <>
+    <Component {...pageProps} />
+    {hasNewDeploy && (
+        <Notification>
+          New version available!
+          <button onClick={() => window.location.reload()}>Refresh</button>
+        </Notification>
+      )}
+     </>
+    )
 }
 
 export default MyApp
