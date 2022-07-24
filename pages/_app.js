781e8b5745561a5d366baf701c110813ce0f37be
Jitumoni changkakoty <g2.genious@gmail.com>
2022-07-24 14:05:01 +0530

    test version

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import { useHasNewDeploy } from "next-deploy-notifications";
+import {useHasNewDeploy} from "next-deploy-notifications";
 
 function MyApp({ Component, pageProps }) {
   let { hasNewDeploy } = useHasNewDeploy();
