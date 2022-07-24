f48fea2b459821fa38a711eb5204075d0e4b421e
Jitumoni changkakoty <g2.genious@gmail.com>
2022-07-24 19:33:06 +0530

    og tags addes

--- components/common/Heads.jsx
+++ components/common/Heads.jsx
@@ -5,11 +5,14 @@ const Heads =()=>{
     <meta charSet="UTF-8"/>
     <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Ziro Festival of Music : Encamp Adventures</title>
     <meta name="description" content="If you want to travel in Northeast India, talk to Encamp Adventures - the leading camping service providers in Assam, Meghalaya, Arunachal Pradesh, Nagaland; we offer customized trips in the region."/>
     <meta name="keywords" content="Encamp Adventures, camping, camping provider, luxury camping, camping places, camping trip, adventure, adventure tourism, trekking, trekking in india, north east india tourism, kayaking, cliff jumping, recreational activities"/>
-    <title>Ziro Festival of Music : Encamp Adventures</title>
+    <meta property="og:title" content="Ziro Festival of Music : Encamp Adventures" />
+    <meta property="og:description" content="Experience the multiverse of music, culture, joy, and eco-friendly living at Ziro only with Encamp." />
+    <meta property="og:image" content="/images/mobile.jpg" />
     <link rel="icon" type="image/png" href="/images/icons/favicon-32x32.png" sizes="32x32"/>
-     </Head>
+    </Head>
           )
 }
 export default Heads
