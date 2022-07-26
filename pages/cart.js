20e38aa09b8fa354b0a07056817cf8b6f40952d4
Jitumoni changkakoty <g2.genious@gmail.com>
2022-07-26 15:03:27 +0530

    generate_lead

--- pages/cart.js
+++ pages/cart.js
@@ -152,7 +152,7 @@ if(localresponse.data !== 400){
                     isadvance:data.isadvance
 
                   });
-Tagmanageri(ordertotal,'generate_lead');
+Tagmanageri(data.ordertotal,'generate_lead');
         
         let uniqueid = localStorage.getItem('cartid');
         localStorage.removeItem(uniqueid);
